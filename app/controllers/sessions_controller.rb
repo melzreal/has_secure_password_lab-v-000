375a7dc6f53de645326950382a8b06b06b7a07e2
Melissa Real <melissa.real@hotmail.com>
2019-02-13 22:12:18 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,12 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(name: params[:user][:name])
-   if @user && @user.authenticate(params[:password])
+   if @user.authenticate(params[:password])
         session[:user_id] = @user.id
         redirect_to hello_path
     else
         redirect_to login_path
     end
-
  end
+
 end
