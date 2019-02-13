d08bb983323f7b4afab2c742335e080f7bdc7e54
Melissa Real <melissa.real@hotmail.com>
2019-02-13 21:36:14 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,12 @@ class SessionsController < ApplicationController
 
   def create
    @user = User.find_by(name: params[:name])
-   return head(:forbidden) unless @user.authenticate(params[:password])
-   session[:user_id] = @user.id
+
+   if @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+    else
+        redirect_to '/login'
+    end
+
  end
 end
