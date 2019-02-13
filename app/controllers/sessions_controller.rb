d9e42e4f0bd30137164635bd25c2a12b87949dbf
Melissa Real <melissa.real@hotmail.com>
2019-02-13 22:23:53 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
         session[:user_id] = @user.id
         redirect_to hello_path
     else
-        redirect_to login_path
+        redirect_to new_session_path
     end
  end
 
