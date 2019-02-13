1c54196ab66f28e83e0d0c7bb3778fe42c519a5e
Melissa Real <melissa.real@hotmail.com>
2019-02-13 22:32:30 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    binding.pry
+
     @user = User.find_by(name: params[:user][:name])
-   if @user.authenticate(params[:password])
+   if @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
-        redirect_to hello_path
+      redirect_to hello_path
     else
         redirect_to new_session_path
     end
