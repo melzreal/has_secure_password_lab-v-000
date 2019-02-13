20685394d21774b2b4183a958cdf2d25a71ca0be
Melissa Real <melissa.real@hotmail.com>
2019-02-13 22:29:49 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   end
 
   def create
+    binding.pry
     @user = User.find_by(name: params[:user][:name])
    if @user.authenticate(params[:password])
       session[:user_id] = @user.id
