ecd2b5406b743659406aaafc05194ca055d8d986
Melissa Real <melissa.real@hotmail.com>
2019-02-13 21:59:59 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
 
    if @user.authenticate(params[:password])
         session[:user_id] = @user.id
+        redirect_to hello_path
     else
         redirect_to login_path
     end
